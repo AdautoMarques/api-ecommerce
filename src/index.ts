be2afc63eaa779454038569c1519fa40d436829d
Adauto Marques <adautomarques937@gmail.com>
2024-11-13 18:12:27 -0300

    Instalando e configurando o ts-watch

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Word");
+  res.send("Hello Word!!");
 });
 
 app.listen(3333, () => {
-  console.log("Servidor rodando na Porta 3333");
+  console.log("Servidor rodando na Porta 3000");
 });
