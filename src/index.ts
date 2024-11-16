4d8139299368fc1a4cd9984b9728b4faaa218330
Adauto Marques <adautomarques937@gmail.com>
2024-11-15 21:32:03 -0300

    Configurando as rotas

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Word!!");
-});
-
-app.get("/users", (req: Request, res: Response) => {
-  let usuarios = [
-    {
-      nome: "Adauto",
-      idade: 30,
-    },
-    {
-      nome: "Matteo",
-      idade: 3,
-    },
-  ];
-  res.send(usuarios);
-});
+routes(app);
 
 app.listen(3333, () => {
-  console.log("Servidor rodando na Porta 3000");
+  console.log("Servidor rodando na Porta 3333!!");
 });
